644d34ca9dc6b0ca7015d156ed39a4a079db224f
Potesz Attila <hungaryblacklegion@gmail.com>
2020-10-20 17:34:57 +0200

    Add project files.

---
+++ 2020.10.20 munka/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020._10._20_munka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] t = new int[10];
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                Console.WriteLine(r.Next(10, 100));
+            }
+            Console.ReadKey();
+        }
+    }
+}
